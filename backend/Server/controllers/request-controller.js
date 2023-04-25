f3a073a4b33f3b2e784ba96cc36c31f45757f067
stefanocando <stefano.cando@uc.cl>
2023-04-25 11:07:30 -0400

    post a broker

--- backend/Server/controllers/request-controller.js
+++ backend/Server/controllers/request-controller.js
@@ -1,27 +1,50 @@
 const db = require('../models/index');
 const requests = require('../models/event');
 const HttpError = require('../http-error');
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
 
 
 const createRequest = async(req, res, next) =>{
-  const {group_id, event_id, deposit_token, quantity, seller, user_id, state } =
+  const {group_id, event_id, deposit_token, quantity, seller, user_id} =
     req.body;
+  const uuid = uuidv4();
+  const request_data = {
+    "request_id": uuid,
+    "group_id": group_id, 
+    "event_id": event_id, 
+    "deposit_token": deposit_token, 
+    "quantity": quantity, 
+    "seller" : ""
+  };
 
-  const createdRequest = db.Request.build({
-    group_id: group_id,
-    deposit_token: deposit_token,
-    quantity, quantity,
-    seller: seller,
-    user_id: user_id,
-    event_id: event_id,
-  })
   try {
-    await createdRequest.save();
-    res.json({messaje: 'Succesfully created Request'})
+    const request_response = await axios.post('http://localhost:9000/requests', {
+      request_data
+    }, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    request_response.then((response) => {console.log(response.data)})
   } catch (err) {
-    const error = new HttpError('Could not create event', 500);
-    return error;
+    console.log(err);
   }
+
+  // const createdRequest = db.Request.build({
+  //   group_id: group_id,
+  //   deposit_token: deposit_token,
+  //   quantity, quantity,
+  //   seller: seller,
+  //   user_id: user_id,
+  //   event_id: event_id,
+  // })
+  // try {
+  //   await createdRequest.save();
+  //   await 
+  //   res.json({messaje: 'Succesfully created Request'})
+  // } catch (err) {
+  //   const error = new HttpError('Could not create event', 500);
+  //   return error;
+  // }
 }
 
 exports.createRequest = createRequest;
