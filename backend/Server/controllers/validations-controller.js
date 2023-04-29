c96d22b7be3f719abfb0b31c06015230dd6b4564
stefanocando <stefano.cando@uc.cl>
2023-04-28 20:49:09 -0400

    conexiones internas backend

--- backend/Server/controllers/validations-controller.js
+++ backend/Server/controllers/validations-controller.js
@@ -4,14 +4,20 @@ const HttpError = require('../http-error');
 
 const updateRequest = async (req, res, next) => {
     const { request_id, group_id, seller, valid } = req.body.info;
-    let request;
-    try {
-      request = await db.Request.findOne({ where: { request_id: request_id } })
-      .then((request) => {request.valid = valid; request.save(); res.json({message: 'Request validated!'})});
-    } catch (err) {
-      const error = new HttpError('Could not find request', 500);
-      return error;
+    const request = await db.Request.findOne({ where: { request_id: request_id } })
+    if (request === null) {
+      res.json({message: 'Request not found!'});
     }
-  };
+    else {
+      try {
+        request.valid = valid;
+        await request.save();
+        res.json({message: 'Request validated!'});
+      } catch (err) {
+        const error = new HttpError('Could update request status.', 500);
+        return error;
+      }
+    }
+}
 
 exports.updateRequest = updateRequest;
