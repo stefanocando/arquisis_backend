0fa1ffdbbe553ba7a5d8dd2fe1034ea37aff8ac8
stefanocando <stefano.cando@uc.cl>
2023-04-30 16:16:48 -0400

    fix cors

--- backend/Server/app.js
+++ backend/Server/app.js
@@ -17,17 +17,13 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader(
-    'Access-Control-Allow-Mehtods',
-    'GET',
-    'POST',
-    'PATCH',
-    'DELETE'
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE'
   );
   next();
 });
