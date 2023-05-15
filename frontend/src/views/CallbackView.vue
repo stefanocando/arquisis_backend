dd51a899ce4acf32fa972333385c23444000bbda
Nicólas Abarca <nicoabarca@uc.cl>
2023-05-15 08:58:32 -0400

    chore: remove frontend

--- frontend/src/views/CallbackView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="d-flex justify-content-center align-items-center vh-100">
-    <div class="text-center spinner-border" role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
-  </div>
-</template>
